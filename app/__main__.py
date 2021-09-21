0383a4bdef706160267081fd2fe5845951e45c1f
Sergey Kovalevich <inndie@gmail.com>
2021-09-22 01:31:44 +0300

    aaaaa

--- app/__main__.py
+++ app/__main__.py
@@ -1,7 +1,8 @@
 import logging
 import asyncio
 
-from aiogram.types import BotCommand
+from aiogram.types import BotCommand, ParseMode
+from aiogram import Bot, Dispatcher
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
@@ -9,24 +10,41 @@ logging.basicConfig(
 
 log = logging.getLogger(__name__)
 
+from app.handlers.commands import register_commands
 from app.config import config
-from app.handlers import *
-from app.core import bot, dp, commands, notify_message, notify_sticker
+from app.core import commands
 
-async def start_telegram_bot() -> None:
+def get_notify_chats():
+    yield from config.telegram.chats
+
+async def notify_message(dp: Dispatcher, text: str):
+    for chat_id in get_notify_chats():
+        try:
+            await dp.bot.send_message(chat_id, text)
+        except:
+            log.exception(f"failed to send message to chat_id={chat_id}")
+
+async def notify_sticker(dp: Dispatcher, sticker_id: str):
+    for chat_id in get_notify_chats():
+        try:
+            await dp.bot.send_sticker(chat_id, sticker_id)
+        except:
+            log.exception(f"failed to send sticker to chat_id={chat_id}")
+
+async def start_telegram_bot(dp: Dispatcher) -> None:
     log.info("starting telegram bot")
 
     await dp.skip_updates()
 
-    await bot.set_my_commands(
+    await dp.bot.set_my_commands(
         [BotCommand(command=command, description=description) for command, description in commands]
     )
 
     greeting_sticker = config.stickers.get("greeting", None)
     if greeting_sticker is None:
-        await notify_message("\N{Robot Face} bot online")
+        await notify_message(dp, "\N{Robot Face} bot online")
     else:
-        await notify_sticker(greeting_sticker)
+        await notify_sticker(dp, greeting_sticker)
 
     await dp.start_polling()
 
@@ -35,8 +53,13 @@ def main() -> None:
 
     loop = asyncio.get_event_loop()
 
+    bot = Bot(token=config.telegram.token, parse_mode=ParseMode.HTML)
+    dp = Dispatcher(bot)
+
+    register_commands(dp, chat_id=config.telegram.chats)
+
     try:
-        asyncio.ensure_future(start_telegram_bot())
+        asyncio.ensure_future(start_telegram_bot(dp))
         loop.run_forever()
     except KeyboardInterrupt:
         log.info("CTRL-C")
--- app/core.py
+++ app/core.py
@@ -1,14 +1,4 @@
-__all__ = ("bot", "dp", "commands", "notify_message", "notify_sticker")
-
-import logging
-
-from aiogram import Bot, Dispatcher
-from aiogram.types import ParseMode
-
-from .config import config
-
-bot = Bot(token=config.telegram.token, parse_mode=ParseMode.HTML)
-dp = Dispatcher(bot)
+__all__ = ("commands")
 
 # bot commands
 commands = (
@@ -16,22 +6,3 @@ commands = (
     ('/photo',  'Capture and send photo'),
     ('/video',  'Capture and send video'),
 )
-
-log = logging.getLogger(__name__)
-
-def get_notify_chats():
-    yield from config.telegram.chats
-
-async def notify_message(text: str):
-    for chat_id in get_notify_chats():
-        try:
-            await bot.send_message(chat_id, text)
-        except:
-            log.exception(f"failed to send message to chat_id={chat_id}")
-
-async def notify_sticker(sticker_id: str):
-    for chat_id in get_notify_chats():
-        try:
-            await bot.send_sticker(chat_id, sticker_id)
-        except:
-            log.exception(f"failed to send sticker to chat_id={chat_id}")
