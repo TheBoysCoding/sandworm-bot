4965de248103182c1b73bead8307812462cd7bc5
Sergey Kovalevich <inndie@gmail.com>
2021-11-12 22:05:42 +0300

    fix issue with typing

--- app/handlers/main_group_start.py
+++ app/handlers/main_group_start.py
@@ -1,8 +1,9 @@
+from typing import List
 from aiogram.types import Message, BotCommand
 
 from app.misc.command_description import CommandDescription
 
-def register_main_group_start(storage: list[CommandDescription]):
+def register_main_group_start(storage: List[CommandDescription]):
     async def cmd_help(message: Message):
         help_message = "".join(f"/{command.command} - {command.description}\n" for command in storage)
         await message.answer(help_message)
